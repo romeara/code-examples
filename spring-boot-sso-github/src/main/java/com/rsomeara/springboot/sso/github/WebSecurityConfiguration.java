69fa90ca90eca82a5e2da1ff2ff1adbccbfa956a
Ryan O'Meara <romeara@blackducksoftware.com>
2016-10-31 13:41:35 -0400

    Update web security config

--- spring-boot-sso-github/src/main/java/com/rsomeara/springboot/sso/github/WebSecurityConfiguration.java
+++ spring-boot-sso-github/src/main/java/com/rsomeara/springboot/sso/github/WebSecurityConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -25,7 +24,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated().and()
                 .exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login/github")).and()
                 .logout().logoutSuccessUrl("/login/github").permitAll().and()
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
                 .addFilterBefore(gitHubFilter, BasicAuthenticationFilter.class);
     }
 
